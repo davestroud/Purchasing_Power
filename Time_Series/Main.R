7e78c1e8f4dbb2c95775ae9131b841b5f6c8d266
davestroud <david@davidstroud.me>
2019-10-03 17:52:44 -0400

    add new r file

--- Time_Series/Main.R
+++ Time_Series/Main.R
@@ -1,5 +1,6 @@
 
-  Explore the famous River Nile annual streamflow data
+
+# Explore the famous River Nile annual streamflow data:
 
 
 # Print the Nile dataset
@@ -13,7 +14,7 @@ head(Nile, n = 10)
 
 # Display the last 12 elements of the Nile dataset
 tail(Nile, n = 12)
-```
+
 
 # Plot the Nile data
 plot(Nile)
@@ -22,15 +23,21 @@ plot(Nile)
 plot(Nile, xlab = "Year", ylab = "River Volume (1e9 m^{3})")
 
 # Plot the Nile data with xlab, ylab, main, and type arguments
-plot(Nile, xlab = "Year", ylab = "River Volume (1e9 m^{3})", main = "Annual River Nile Volume at Aswan, 1871-1970", type ="b")
+plot(
+  Nile,
+  xlab = "Year",
+  ylab = "River Volume (1e9 m^{3})",
+  main = "Annual River Nile Volume at Aswan, 1871-1970",
+  type = "b"
+)
 
+"The start() and end() functions return the time index of the first and last observations, respectively.
+The time() function calculates a vector of time indices, with one element for each time index on which
+the series was observed.
 
-
-
-The start() and end() functions return the time index of the first and last observations, respectively. The time() function calculates a vector of time indices, with one element for each time index on which the series was observed.
-
-The deltat() function returns the fixed time interval between observations and the frequency() function returns the number of observations per unit time. Finally, the cycle() function returns the position in the cycle of each observation.
-
+The deltat() function returns the fixed time interval between observations and the frequency() function
+returns the number of observations per unit time. Finally, the cycle() function returns the position
+in the cycle of each observation."
 
 # Plot AirPassengers
 plot(AirPassengers)
@@ -39,14 +46,11 @@ plot(AirPassengers)
 start(AirPassengers)
 end(AirPassengers)
 
-# Use time(), deltat(), frequency(), and cycle() with AirPassengers 
+# Use time(), deltat(), frequency(), and cycle() with AirPassengers
 time(AirPassengers)
 deltat(AirPassengers)
 frequency(AirPassengers)
 cycle(AirPassengers)
-
-
-
 
 
 # Plot the AirPassengers data
@@ -63,22 +67,7 @@ plot(AirPassengers)
 
 # Add the complete AirPassengers data to your plot
 rm(AirPassengers)
-points(AirPassengers, type = "l", col = 2, lty = 3)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+points(AirPassengers,
+       type = "l",
+       col = 2,
+       lty = 3)
