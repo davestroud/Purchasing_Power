1d5e76e9092fd072ee524def08c51a54ad5740b1
davestroud <david@davidstroud.me>
2019-10-08 18:15:17 -0400

    add libraries

--- Time_Series/ARIMA.R
+++ Time_Series/ARIMA.R
@@ -1,1 +1,17 @@
-help("AirPassengers")
+library(xts)
+library(astsa)
+
+# View a detailed description of AirPassengers
+help(AirPassengers)
+
+# Plot AirPassengers
+plot(AirPassengers)
+
+
+# Plot the DJIA daily closings
+plot(djia$Close)
+
+# Plot the Southern Oscillation Index
+plot(soi)
+
+
